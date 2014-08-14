c4ce574bafa773247256e9214562ba4785c14913
yinlu <berylyl@126.com>
2014-08-14 12:06:02 +0800

    case updarte

---
+++ src/main/java/jm/testcase/TestA.java
@@ -0,0 +1,24 @@
+package jm.testcase;
+
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+public class TestA {
+
+	@BeforeClass
+	public void setUp() {
+		// close 
+	}
+
+	@Test(groups = { "fast" })
+	public void aFastTest() {
+		Assert.assertEquals("add", "addy");
+		System.out.println("Fast test");
+	}
+
+	@Test(groups = { "slow" })
+	public void aSlowTest() {
+		System.out.println("Slow test");
+	}
+
+}
---
+++ src/main/java/jm/testcase/TestB.java
@@ -0,0 +1,22 @@
+package jm.testcase;
+
+import org.testng.annotations.*;
+
+public class TestB {
+
+	@BeforeClass
+	public void setUp() {
+		// code that will be invoked when this test is instantiated
+	}
+
+	@Test(groups = { "fast" })
+	public void aFastTest() {
+		System.out.println("Fast test");
+	}
+
+	@Test(groups = { "slow" })
+	public void aSlowTest() {
+		System.out.println("Slow test");
+	}
+
+}
